abae422c5b31c298295ca6cd0a5dea5e003836bd
YOHANMAURIN <yohan.david@hotmail.fr>
2017-03-10 15:16:33 +0100

    Update bitcoin.js

--- js/bitcoin.js
+++ js/bitcoin.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$.ajax({
-		url : "https://api.blockcypher.com/v1/btc/main",
+		url : "http://bitcoin.mubiz.com/info",
 		dataType : "json",
 		contentType : "application/json; charset=utf-8",
 		type : "GET",
@@ -8,7 +8,7 @@ $(document).ready(function() {
 		async : false,
 
 		success : function(data) {
-			$('#bitcoin_block_number').append(data.height);
+			$('#bitcoin_block_number').append(data.blocks);
 		},
 
 		error : function(xhr, status, err) {
