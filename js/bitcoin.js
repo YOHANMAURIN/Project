6afbe69ed97772e1beab5f308b062b9458ae2c3a
YOHANMAURIN <yohan.david@hotmail.fr>
2017-04-26 11:25:41 +0200

    Update bitcoin.js

--- js/bitcoin.js
+++ js/bitcoin.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		dataType : "json",
 		contentType : "application/json; charset=utf-8",
 		type : "GET",
-		timeout:	"5000",
+		timeout:"5000",
 		async : false,
 
 		success : function(data) {
@@ -28,17 +28,51 @@ $(document).ready(function() {
 
 		success : function(data) {
 			$('#bitcoin_difficulty').append(data.difficulty);
-			$('#bitcoin_connection').append(data.connections);
-			$('#bitcoin_protocol').append(data.protocolversion);
 		},
 
 		error : function(xhr, status, err) {
 			$('#bitcoin_difficulty').append(err+" N/A");
-			$('#bitcoin_connection').append(err+" N/A");
-			$('#bitcoin_protocol').append(err+" N/A");
 		}
 	});
 });
 
 
+$(document).ready(function() {
+	$.ajax({
+		url : "https://bitcoin.mubiz.com/info",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout: "5000",
+		async : false,
+
+		success : function(data) {
+			$('#bitcoin_connection').append(data.connections);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_connection').append(err+" N/A");
+		}
+	});
+});
+
+
+$(document).ready(function() {
+	$.ajax({
+		url : "https://bitcoin.mubiz.com/info",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout: "5000",
+		async : false,
+
+		success : function(data) {
+			$('#bitcoin_protocol').append(data.protocolversion);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_protocol').append(err+" N/A");
+		}
+	});
+});
 
