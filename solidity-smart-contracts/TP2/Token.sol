d4d4cf1efb8503736d9272e8188375d83d5934fa
YOHANMAURIN <yohan.david@hotmail.fr>
2018-02-20 18:47:16 +0100

    Update Token.sol

--- solidity-smart-contracts/TP2/Token.sol
+++ solidity-smart-contracts/TP2/Token.sol
@@ -1,10 +1,13 @@
 pragma solidity ^0.4.0;
 
 contract newToken {
-    mapping (address => uint256) public balanceOf;
-    uint256 supply;
     address _to;
     uint256 _value;
+    uint256 supply;
+    mapping(address => uint256) balances;
+    mapping (address => mapping (address => uint256)) internal allowed;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
     
     function newToken(uint256 supply) public {
         //uint256 supply = 50; si on veut le rentrer en dure le nombre de token
@@ -21,4 +24,17 @@ contract newToken {
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
     }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[_from]);
+    require(_value <= allowed[_from][msg.sender]);
+
+    balances[_from] -= _value;
+    balances[_to] += _value;
+    allowed[_from][msg.sender] +=_value;
+    Transfer(_from, _to, _value);
+    return true;
+  }
 }
+
