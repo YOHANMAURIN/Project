9de8dc14837e97e6b99962f023b0b8228407a1e3
YOHANMAURIN <yohan.david@hotmail.fr>
2018-06-30 15:58:43 +0200

    Update DistribGemmes.sol

--- Delinquants/DistribGemmes.sol
+++ Delinquants/DistribGemmes.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.21;
-import "./TokenERC20skin.sol";
+import "./Gemmes.sol";
 
 contract UBSav {
     
-    uint32 x; //récupèré via un serveur
+    uint32 x; //récupèré via un server 
     uint32 y; 
     uint32 z;
     address player;
@@ -17,23 +17,23 @@ contract UBSav {
     event Transfer(address _to, uint256 value);
 
     function SendCoin(uint32 _x, uint32 _y, uint32 _z, address _player) public {//now est le timestamp du bloc actuel en secondes depuis l'époque unix
-        if (now>1528298491){//https://www.epochconverter.com/ pour trouver le chiffre correspondant à la date 
-            if (now<1528298491 + 7 days){ //date de fin si la campagne dure 7 jours (weeks,minutes, seconds fonctionne)
-                if(msg.sender==0x95Ea183985ea4F5f2a18bf07dB37eB401e768c73){ //msg.sender celui qui est entrain d'intéragir avec le SC
+        if (now>1530364991){//https://www.epochconverter.com/ pour trouver le chiffre correspondant à la date 
+            if (now<1530364991 + 2 days){ //date de fin si la campagne dure 2 jours (weeks,minutes, seconds fonctionne)
+                if(msg.sender==0x95Ea183985ea4F5f2a18bf07dB37eB401e768c73){ 
                     x = _x;
                     y = _y;
                     z =_z;
                     player = _player;
                     if ( x1<x && x<x2 && y1<y && y<y2 && z1<z && z<z2){
-                        Ubisoft(0xBc1A01ef83Ee7e3A3AB9A9BA67c56f4Fba2166A9 ).transfer(_player,value); //adresse tokenERC20skin
+                        Gemmes(0x8cE0e4CeD204FFC5D01493879ACd418B9f8862fe ).transfer(_player,value); //adresse tokenERC20skin
                         emit Transfer(_player,value);
                     }
                 }
             }
             else{
                 address ubisoft=0x95Ea183985ea4F5f2a18bf07dB37eB401e768c73; 
-                uint tokenBalance = Ubisoft(0xc7b5822eA575806C6740Fe7fc38C8f86142f6B54).balanceOf(address(this));
-                Ubisoft(0xBc1A01ef83Ee7e3A3AB9A9BA67c56f4Fba2166A9 ).transfer(ubisoft,tokenBalance);
+                uint tokenBalance = Gemmes(0x8cE0e4CeD204FFC5D01493879ACd418B9f8862fe).balanceOf(address(this));
+                Gemmes(0x8cE0e4CeD204FFC5D01493879ACd418B9f8862fe ).transfer(ubisoft,tokenBalance);
                 emit Transfer(ubisoft,tokenBalance);
                 selfdestruct(ubisoft);//kill le contract et renvoie les éthers du SC à Ubisoft
             }
