75ea46f3c81766ca3fb15383f59a81a5331d8d0e
YOHANMAURIN <yohan.david@hotmail.fr>
2018-07-01 14:30:37 +0200

    Update Vote.sol

--- Delinquants/Vote.sol
+++ Delinquants/Vote.sol
@@ -7,33 +7,28 @@ contract Voting {
     {
     address addressparticipants;
     uint8 role; //0 recrue, 1 chef 
-    address vote_contre;
+    bool decision;
     }
     
     Joueurs[] public liste_joueurs; // la liste s'initialise à 0
     
     address public owner;
     function owned() public { owner = msg.sender; }
-
     
-    mapping(address => uint) public vote_contre;
-    
-    function vote_chef(address voteChoice) public //Voter pour éliminer une personne 
+    function vote_chef(bool voteChoice) public //Voter pour éliminer une personne 
     {
         uint indice = nombre_de_joueur;
         while (indice>0)
         {
             if (liste_joueurs[indice].role==1)
             {
-               //if(voteChoice != liste_participants.length) throw;
                 liste_joueurs.push( Joueurs({
                     addressparticipants : msg.sender,
                     role : 1,
-                    vote_contre: voteChoice
+                    decision: voteChoice
                     
                         })
                     );
-                    vote_contre [voteChoice]+=1;
             }
             indice=indice -1;
         }
@@ -41,7 +36,7 @@ contract Voting {
     }
     
     
-    function vote_all (address voteChoice) public  //lorsque l'une personne veut rentrer dans le clan
+    function vote_all (bool voteChoice) public  //lorsque l'une personne veut rentrer dans le clan
     {
         uint indice = nombre_de_joueur;
         while (indice>0)
@@ -51,11 +46,10 @@ contract Voting {
                 liste_joueurs.push( Joueurs({
                     addressparticipants : msg.sender,
                     role : 0,
-                    vote_contre: voteChoice
+                    decision: voteChoice
                     
                         })
                     );
-                vote_contre [voteChoice]+=1;
             }
             if (liste_joueurs[indice].role==1)
             {
@@ -63,11 +57,10 @@ contract Voting {
                     addressparticipants : msg.sender,
 
                     role : 1,
-                    vote_contre: voteChoice
+                    decision: voteChoice
                     
                         })
                     );
-                    vote_contre [voteChoice]+=1;
             }
             indice=indice -1;
         }
