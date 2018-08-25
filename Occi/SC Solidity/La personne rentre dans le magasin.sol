b4d39d68d170fdaf0c17d80d566892e2d00ca796
YOHANMAURIN <yohan.david@hotmail.fr>
2018-08-25 15:47:51 +0200

    Update La personne rentre dans le magasin.sol

--- Occi/SC Solidity/La personne rentre dans le magasin.sol
+++ Occi/SC Solidity/La personne rentre dans le magasin.sol
@@ -1,10 +1,12 @@
-pragma solidity 0.4.21;
-import "./TokenTest.sol";
+pragma solidity ^0.4.21;
+import "./ForeignBridge.sol";
 
-contract URPav {
-    
+ 
+
+contract URP {
+
     uint32 x; //récupèré via l’API Occi
-    uint32 y; 
+    uint32 y;
     uint32 z;
     address shopper;
     uint32 x1=9000000;
@@ -13,42 +15,41 @@ contract URPav {
     uint32 y2=35000000;
     uint16 z1=200;
     uint16 z2=400;
-    uint128 value = 10*(10**18);
-    address retailer=0x48a484a13BB7b08C8F0022C538E871cb4417E5c9; 
+    uint128 value = 1*(10**18);
+    address retailer=0x005Eb3D86d6C987860FBC95AB97DA69Ee6b6118E;
     event Transfer(address _to, uint256 value);
 
     function Refund() public
     {
-        if (msg.sender==0x95Ea183985ea4F5f2a18bf07dB37eB401e768c73){
-            uint tokenBalance = Test(0xc7b5822eA575806C6740Fe7fc38C8f86142f6B54).balanceOf(address(this));
-            Test(0xc7b5822eA575806C6740Fe7fc38C8f86142f6B54).transfer(retailer,tokenBalance);
+        if (msg.sender==0x005Eb3D86d6C987860FBC95AB97DA69Ee6b6118E){
+            uint tokenBalance = ForeignBridge(0xfe989a69356bab1b35e73d3d7d02ee5310e19005).balanceOf(address(this));
+            ForeignBridge(0xfe989a69356bab1b35e73d3d7d02ee5310e19005).transfer(retailer,tokenBalance);
             emit Transfer(retailer,tokenBalance);
             selfdestruct(retailer);//kill le contract et renvoie les éthers du SC au retailer
-        }         
+        }   
     }
 
     function SendCoin(uint32 _x, uint32 _y, uint32 _z, address _shopper) public {//now est le timestamp du bloc actuel en secondes depuis l'époque unix
-        if (now>1528298491){//https://www.epochconverter.com/ pour trouver le chiffre correspondant à la date 
-            if (now<1528298491 + 7 days){ //date de fin si la campagne dure 7 jours (weeks,minutes, seconds fonctionne)
-                if(msg.sender==0x95Ea183985ea4F5f2a18bf07dB37eB401e768c73){ //msg.sender celui qui est entrain d'intéragir avec le SC
+        if (now>1535202185){//https://www.epochconverter.com/ pour trouver le chiffre correspondant à la date
+            if (now<1535202185 + 7 days){ //date de fin si la campagne dure 7 jours (weeks,minutes, seconds fonctionne)
+                if(msg.sender==0x005Eb3D86d6C987860FBC95AB97DA69Ee6b6118E){ //msg.sender celui qui est entrain d'intéragir avec le SC
                     x = _x;
                     y = _y;
                     z =_z;
                     shopper = _shopper;
                     if ( x1<x && x<x2 && y1<y && y<y2 && z1<z && z<z2){
-                        Test(0xc7b5822eA575806C6740Fe7fc38C8f86142f6B54).transfer(_shopper,value);
+                        ForeignBridge(0xfe989a69356bab1b35e73d3d7d02ee5310e19005).transfer(_shopper,value);
                         emit Transfer(_shopper,value);
                     }
                 }
             }
             else{
-               Refund(); 
+               Refund();
             }
         }
    }
-   
+
    function getSendCoin() public constant returns (uint32, uint32, uint32, address) {
        return (x, y, z, shopper);
    }
-    
 }
