37218f919c7e43d8f00cebb77ac9077ee7a776de
Yohan M <yohan.david@hotmail.fr>
2018-12-09 12:33:37 +0100

    Update DistribGemmes.sol

--- Ubisoft_Hackathon/DistribGemmes.sol
+++ Ubisoft_Hackathon/DistribGemmes.sol
@@ -1,14 +1,16 @@
-// adresse contrat https://ropsten.etherscan.io/address/0x388cd4db6f12a5e51cc6222b39200f79f439e092
+https://ropsten.etherscan.io/address/0xb881edb3fe528ff9412dc364e86685ac2beeb410
 
 pragma solidity ^0.4.21;
-import "./Gemmes.sol";
+import "./ERC20Gemmes.sol";
 
 contract UBSav {
     
-    uint32 x; //récupèré via un server 
+    //Retrieved via a server
+    uint32 x;
     uint32 y; 
     uint32 z;
     address player;
+    //Set relative to the place of the distribution of the gemmes
     uint32 x1=9000000;
     uint32 x2=11000000;
     uint32 y1=33000000;
@@ -16,28 +18,29 @@ contract UBSav {
     uint16 z1=200;
     uint16 z2=400;
     uint128 value = 10*(10**18);
+    
     event Transfer(address _to, uint256 value);
 
-    function SendCoin(uint32 _x, uint32 _y, uint32 _z, address _player) public {//now est le timestamp du bloc actuel en secondes depuis l'époque unix
-        if (now>1530364991){//https://www.epochconverter.com/ pour trouver le chiffre correspondant à la date 
-            if (now<1530364991 + 2 days){ //date de fin si la campagne dure 2 jours (weeks,minutes, seconds fonctionne)
+    function SendCoin(uint32 _x, uint32 _y, uint32 _z, address _player) public {//now is the timestamp of the current block in seconds since unix time
+        if (now>1544353261){//https://www.epochconverter.com/ to find the number corresponding to the date
+            if (now<1544353261 + 50 weeks){ //end date if the campaign lasts 50 weeks(days,minutes, seconds work)
                 if(msg.sender==0x95Ea183985ea4F5f2a18bf07dB37eB401e768c73){ 
                     x = _x;
                     y = _y;
                     z =_z;
                     player = _player;
                     if ( x1<x && x<x2 && y1<y && y<y2 && z1<z && z<z2){
-                        Gemmes(0xD4008fAEdb116C166EacE4cDf4CBb7853df7C519).transfer(_player,value); //adresse tokenERC20 gemmes
+                        Gemmes(0xe02005819E60b16E3eBaa040df7d3394AF9AC12e).transfer(_player,value); //address tokenERC20 gemmes
                         emit Transfer(_player,value);
                     }
                 }
             }
             else{
                 address ubisoft=0x95Ea183985ea4F5f2a18bf07dB37eB401e768c73; 
-                uint tokenBalance = Gemmes(0xD4008fAEdb116C166EacE4cDf4CBb7853df7C519).balanceOf(address(this));
-                Gemmes(0xD4008fAEdb116C166EacE4cDf4CBb7853df7C519 ).transfer(ubisoft,tokenBalance);
+                uint tokenBalance = Gemmes(0xe02005819E60b16E3eBaa040df7d3394AF9AC12e).balanceOf(address(this));
+                Gemmes(0xe02005819E60b16E3eBaa040df7d3394AF9AC12e ).transfer(ubisoft,tokenBalance);
                 emit Transfer(ubisoft,tokenBalance);
-                selfdestruct(ubisoft);//kill le contract et renvoie les éthers du SC à Ubisoft
+                selfdestruct(ubisoft);//kill the contract and return the tokens of the contract to Ubisoft
             }
         }
    }
